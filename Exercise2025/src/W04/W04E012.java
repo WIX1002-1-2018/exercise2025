086d42e8936c55c861ad568ec7fdcb6b1788666c
Badrul Anuar <askbard@gmail.com>
2025-04-24 13:35:39 +0800

    commit

---
+++ Exercise2025/src/W04/W04E012.java
@@ -0,0 +1,13 @@
+
+public class W04E012 {
+
+    public static void main(String[] args) {
+   
+        System.out.println("Hello \nWorld ");
+        System.out.println("Hello \nWorld ");
+        System.out.println("Hello \nWorld ");
+        System.out.println("Hello \nWorld ");
+        System.out.println("Hello \nWorld ");
+    }
+    
+}
