ba21ece13f3721c57b14af0ff2a48006edec83a6
Badrul Anuar <askbard@gmail.com>
2025-04-24 13:44:01 +0800

    commint

--- Exercise2025/src/W04/W04E012.java
+++ Exercise2025/src/W04/W04E012.java
@@ -2,12 +2,16 @@
 public class W04E012 {
 
     public static void main(String[] args) {
-   
         System.out.println("Hello \nWorld ");
         System.out.println("Hello \nWorld ");
         System.out.println("Hello \nWorld ");
         System.out.println("Hello \nWorld ");
         System.out.println("Hello \nWorld ");
+        System.out.println("Hello \nWorld ");
+        
+        for(int i=0;i<10;i++){
+            System.out.println("Hello \nWorld ");
+        }
     }
     
 }
