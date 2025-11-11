d14039f43c5fae7fdffe38a8556f93d1ca05eb49
MUHAMMAD HAZIQ BIN MOHAMMAD KHILMI <mhaziqkhilmi@gmail.com>
2025-11-11 11:44:32 +0800

    question 5

---
+++ Exercise2025/src/W05/W05E05.java
@@ -0,0 +1,23 @@
+package W05;
+import java.util.Scanner;
+public class W05E05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        System.out.print("How many numbers will you enter? : ");
+        num = sc.nextInt();
+
+        int odd=0;
+        int even=0;
+        for(int i=1; i<=num; i++){
+            System.out.print("Enter number " + i + ": ");
+            int input = sc.nextInt();
+            if (input%2==0) even++;
+            else odd++;
+        }
+System.out.println("Total even numbers: " + even);
+System.out.println("Total odd numbers: " + odd);
+        sc.close();
+    }
+}
