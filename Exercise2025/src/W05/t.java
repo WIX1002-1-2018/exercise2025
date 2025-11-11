624fe623e14e02a467987f670a5887444b673448
Cemile-Yaxin <25066696@siswa.um.edu.my>
2025-11-11 18:15:07 +0800

    new

---
+++ Exercise2025/src/W05/t.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package W05;
+
+/**
+ *
+ * @author 38329
+ */
+public class t {
+    
+}
