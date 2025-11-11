8a1e490d9819092233faecc2dad7eb344de49569
24231773-tan <24231773@siswa.um.edu.my>
2025-11-11 13:31:47 +0800

    W05E06

---
+++ Exercise2025/src/W05/W05E06.java
@@ -0,0 +1,16 @@
+package W05;
+import java.util.Scanner;
+public class W05E06 {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter length N:");
+        int n=sc.nextInt();
+        for (int i=0;i<n;i++) {
+            for (int j=0;j<n;j++){
+                System.out.print("P");
+            }
+            System.out.println();
+            
+        }
+    }
+}
