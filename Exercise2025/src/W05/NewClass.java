200be873ee36a79cfebd849ff417b4f183ea4f2c
Cemile-Yaxin <25066696@siswa.um.edu.my>
2025-11-11 12:27:01 +0800

    new class

---
+++ Exercise2025/src/W05/NewClass.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package W05;
+
+/**
+ *
+ * @author 38329
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        
+    }
+}
