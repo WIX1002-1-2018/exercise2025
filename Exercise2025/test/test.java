661d142988c4805b87f473c3db195ae9e993c6e2
星飞 <@Y7000>
2025-11-11 10:46:56 +0800

    test

---
+++ Exercise2025/test/test.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 星飞
+ */
+public class test {
+    public static void main(String[] args) {
+	for(int i=2;i<21;i+=2){
+            System.out.println(i+" ");
+	}
+    }
+}
