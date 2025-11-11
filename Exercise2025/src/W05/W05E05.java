0aec1f188f8c7db8bd437a78fd30ef5c435e3dd5
IvanFong <fongjuntoh@gmail.com>
2025-11-11 11:47:11 +0800

    W05E05 modified

--- Exercise2025/src/W05/W05E05.java
+++ Exercise2025/src/W05/W05E05.java
@@ -9,6 +9,8 @@ public class W05E05 {
 
         int even = 0;
         int odd = 0;
+        int sumodd = 0;
+        int sumeven = 0;
 
         System.out.print("How many numbers will you enter?: ");
         num = sc.nextInt();
@@ -20,11 +22,15 @@ public class W05E05 {
 
             if (number % 2 == 0) {
                 even++;
+                sumeven += number;
             } else {
                 odd++;
+                sumodd += number;
             }
         }
         System.out.println("Total even numbers: " + even);
         System.out.println("Total odd numbers: " + odd);
+        System.out.println("Sum of even numbers: " + sumeven);
+        System.out.println("Sum of odd numbers: " + sumodd);
     }
 }
