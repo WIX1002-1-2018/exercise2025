a731eb836637fe49cefb775e51cc05f4d892ae39
TEH JIA JIE <tehjiajie@gmail.com>
2025-11-11 11:29:21 +0800

    How many times input

--- Exercise2025/src/W05/W05E04.java
+++ Exercise2025/src/W05/W05E04.java
@@ -1,14 +1,19 @@
+//author:tehjiajie
+
 package W05;
 import java.util.Scanner;
 public class W05E04 {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int num;
+    int i=0;
     do{
         System.out.print("Enter a positive number: ");
         num = sc.nextInt();
+        i++;
     }while(num <= 0);
     System.out.println("You entered: " + num);
+    System.out.println("You have entered: " + i + " time(s).");
     sc.close(); 
 }
 }
