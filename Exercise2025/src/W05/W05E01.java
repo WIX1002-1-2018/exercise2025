f25a9400db6738ce5d04429e3ea2a25633566576
jitesh1059 <25006745@siswa.um.edu.my>
2025-11-11 10:21:44 +0800

    done w05e01

---
+++ Exercise2025/src/W05/W05E01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package W05;
+
+/**
+ *
+ * @author jites
+ */
+public class W05E01 {
+    public static void main(String args[]){
+        for (int i = 1; i <= 20; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
