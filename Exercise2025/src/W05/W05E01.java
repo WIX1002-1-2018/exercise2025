06fa18b7c18b59293433c23c9f5e9e28afeb19b6
Peng-Yi-2025 <24225006@siswa.um.edu.my>
2025-11-11 10:33:09 +0800

    Update W05E01.java

--- Exercise2025/src/W05/W05E01.java
+++ Exercise2025/src/W05/W05E01.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package W05;
 
-/**
- *
- * @author jites
- */
 public class W05E01 {
     public static void main(String args[]){
         for (int i = 1; i <= 20; i++){
@@ -15,6 +7,5 @@ public class W05E01 {
                 System.out.println(i);
             }
         }
-    }
-    
+    }    
 }
