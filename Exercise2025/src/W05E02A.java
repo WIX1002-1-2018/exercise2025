a5dbabe339557cb1afb83f159812c71e4722c880
IvanFong <fongjuntoh@gmail.com>
2025-11-11 10:52:28 +0800

    W05E02A done

--- Exercise2025/src/W05E02A.java
+++ Exercise2025/src/W05E02A.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
 
-public class W05E02 {
+public class W05E02A {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num;
         int sum = 0;
-        while (true) {
-            System.out.print("Enter a number: (0 to quit): ");
+        do {
+            System.out.println("Enter a number: (0 to quit): ");
             num = sc.nextInt();
-            if (num == 0) {
-                break;
+            if (num != 0) {
+                sum += num;
             }
-            sum += num;
-
-        }
+        } while (num != 0);
         System.out.println("The total sum is: " + sum);
     }
 }
