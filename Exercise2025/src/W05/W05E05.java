0956ed3297874fe84d2acffbf854d8253be3d611
AliJeyawiSCME <22310023@scme.edu.ps>
2025-11-11 11:46:37 +0800

    last commit

--- Exercise2025/src/W05/W05E05.java
+++ Exercise2025/src/W05/W05E05.java
@@ -8,17 +8,24 @@ public class W05E05 {
         System.out.print("How many number will you enter?: ");
         int i = scanner.nextInt();
         int ecounter = 0;
+        int esum = 0;
         int ocounter = 0;
+        int osum = 0;
         for (int j = 0; j < i; j++) {
             System.out.printf("Enter number %d:", j + 1);
             int num = scanner.nextInt();
             if (num % 2 == 0) {
                 ecounter += 1;
+                esum += num;
             } else if (num % 2 != 0) {
                 ocounter += 1;
+                osum += num;
             }
         }
         System.out.println("Total even number: " + ecounter);
+        System.out.println("Sum of even numbers: " + esum);
         System.out.println("Total odd number: " + ocounter);
+        System.out.println("Sum of odd numbers: " + osum);
+
     }
 }
