6a195c2e9c83fa09cd0fdf5acf85eae167a822fc
TJY_6666 <25005715@siswa.um.edu.my>
2025-11-11 10:57:09 +0800

    lecture question

--- Exercise2025/src/W05/W05E02A.java
+++ Exercise2025/src/W05/W05E02A.java
@@ -2,17 +2,17 @@ package W05;
 import java.util.Scanner;
 /**
  *
- * @author tanjunyong
+ * 
  */
 public class W05E02A {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int sum = 0;
+        double sum = 0, num = 0;
         do{
-            System.out.print("Please enter integer : ");
-            int num = input.nextInt();
+            System.out.print("Enter a number : ");
+            num = input.nextDouble();
             sum += num;
-        }while (sum!=0);
+        }while (num!=0);
         System.out.println("Sum: "+sum);
     }
     
