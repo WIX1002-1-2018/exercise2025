f34656434097b920e79092a4d546210efba5ddd6
Frederic Chiaw Hui Chuan <25065802@siswa.um.edu.my>
2025-11-11 12:38:16 +0800

    Add files via upload

---
+++ W05E04.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class W05E04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive number:");
+        int a;
+        int b=0;
+        do{
+            a=sc.nextInt();
+            if(a<=0){
+                System.out.print("Please enter a positive number:");
+            }
+            b++;
+        }while(a<=0);
+        System.out.printf("You entered:%d\n",a);
+        System.out.printf("You have entered %d times\n",b);
+    }
+}
