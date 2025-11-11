185cf5a0b4a8646257c0db46a66779264d5d779d
Peng-Yi-2025 <24225006@siswa.um.edu.my>
2025-11-11 10:51:02 +0800

    Update W05E08.java

--- Exercise2025/src/W05/W05E08.java
+++ Exercise2025/src/W05/W05E08.java
@@ -1,18 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package W05;
 import java.util.Scanner;
-/**
- *
- * @author pensyarah
- */
+
 public class W05E08 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner scanner = new Scanner(System.in);
@@ -20,9 +12,15 @@ public class W05E08 {
         String word1 = scanner.nextLine();
         
         for ( int i = word1.length() - 1; i >= 0; i--){
-            System.out.print(word1.charAt(i));
+            do{System.out.print(word1.charAt(i));
         }
+                while{
+                System.outprint(null);
+                
+                }
+            
         System.out.println();
-    }
+    
     
 }
+
