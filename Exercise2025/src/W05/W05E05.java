dc09e5f4452ba5fab1921d202bcf7fc0495a28df
TJY_6666 <25005715@siswa.um.edu.my>
2025-11-11 11:47:29 +0800

    lecture question

--- Exercise2025/src/W05/W05E05.java
+++ Exercise2025/src/W05/W05E05.java
@@ -6,18 +6,22 @@ public class W05E05 {
         Scanner input = new Scanner(System.in);
         System.out.print("How many number all you enter? :");
         int count=input.nextInt();
-        int evenCount=0, oddCount=0, currentCount,currentNum;
+        int evenCount=0, oddCount=0, currentCount,currentNum,sumOdd=0,sumEven=0;
         for(currentCount=1;currentCount<=count;currentCount++){
             System.out.print("Enter number "+currentCount+" : ");
             currentNum=input.nextInt();
             if(currentNum%2==0){
                 evenCount++;
+                sumEven += currentNum;
             }else{
                 oddCount++;
+                sumEven += currentNum;
             }
         }
         System.out.println("Total even number : " + evenCount);
-        System.out.println("Total odd number : " + oddCount);
+        System.out.println("Total odd number : " + oddCount);       
+        System.out.println("Sum of even number : " + sumEven);
+        System.out.println("Sum of odd number : " + sumOdd);
     }
     
 }
