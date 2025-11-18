f2613c3b76c0541b90cded5699ee9be37e89c23a
Badrul Anuar <askbard@gmail.com>
2025-11-18 06:50:08 +0000

    modified:   Week06 Files by Prof Badrul

--- Exercise2025/src/W06/W06E03.java
+++ Exercise2025/src/W06/W06E03.java
@@ -1,25 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package W06;
 
 /**
- *
+ * W06E03 - Working with Large Arrays
+ * Demonstrates: Creating larger arrays and accessing specific elements
+ * 
+ * Key Concepts:
+ * - Array size can be any positive integer
+ * - Array indexing starts at 0
+ * - Last element is at index (size - 1)
+ * - Uninitialized elements default to 0 for int arrays
+ * 
  * @author pensyarah
+ * @modified Badrul Anuar
  */
 public class W06E03 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Create array with 100 elements (indices 0 to 99)
         int [] num = new int[100];
+        
+        // Set first element (index 0)
         num[0]=12;
+        
+        // Set last element (index 99)
         num[99]=89;
-        System.out.println(num[0]);
-        System.out.println(num[99]);
+        
+        // Display first and last elements
+        System.out.println(num[0]);   // Prints 12
+        System.out.println(num[99]);  // Prints 89
     }
     
 }
