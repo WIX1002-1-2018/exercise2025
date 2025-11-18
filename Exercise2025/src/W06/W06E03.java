3e4c794addf2202c3eb3b069a9fe925c95945dfb
zoeywongzixin <23118969@siswa.um.edu.my>
2025-11-18 11:17:21 +0800

    Add W06E03.java exercise

---
+++ Exercise2025/src/W06/W06E03.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package W06;
+
+/**
+ *
+ * @author User
+ */
+public class W06E03 {
+    public static void main(String[] args) {
+        int [] num = new int[100];
+        num[0]=12;
+        num [99]=89;
+        
+        System.out.println(num[0]);
+        System.out.println(num[99]);
+
+    }
+    
+}
