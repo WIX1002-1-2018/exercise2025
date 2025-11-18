183736c02858898dd2d411dcefb8fd7e7f788ad8
Yaseen Ayatullah Khan <24216496@siswa.um.edu.my>
2025-11-18 11:57:49 +0800

    yaseenkhan (#102)

---
+++ Exercise2025/src/W06/W06E07.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+class W06E07 {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int [] nums = new int[20];
+        for (int index = 0; index < nums.length; index++) {
+            nums[index] = r.nextInt(100);
+            System.out.print(nums[index] + " ");
+        }
+        for (int pass = 1; pass < nums.length; pass++ ) 
+		for ( int i = 0; i < nums.length - 1; i++) 
+			if (nums[ i ] > nums[ i + 1 ])  {
+               		int temp = nums[i];        
+				nums[i] = nums[i+1];  
+				nums[i+1] = temp;
+			}
+		System.out.println("\n" + "Bubble Sort");
+		for (int index = 0; index < nums.length; index++) {
+            System.out.print(nums[index] + " ");
+        }
+    }
+}
