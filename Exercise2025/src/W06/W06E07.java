893770ad1ea526cca93b3dc481a1f4caea907e92
Muhd. Nuqman <abdhakimfnsr@gmail.com>
2025-11-18 11:58:03 +0800

    Create W06E07

---
+++ Exercise2025/src/W06/W06E07.java
@@ -0,0 +1,25 @@
+package W06;
+
+import java.util.Random;
+
+public class W06E07 {
+    public static void main(String[] args) {
+        Random rng = new Random();
+        int[] a = new int[20];
+        for (int i = 0; i < a.length; i++){
+            a[i] = rng.nextInt(100);
+        }
+        for (int pass = 1; pass < a.length; pass++){
+            for (int i = 0; i < a.length - 1; i++){
+                if (a[i] > a[i+1]){
+                    int hold = a[i];
+                    a[i] = a[i+1];
+                    a[i+1] = hold;
+                }
+            }
+        }
+        for (int i = 0; i < a.length; i++){
+            System.out.println(a[i]);
+        }
+    }
+}
