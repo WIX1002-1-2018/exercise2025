1e9035c6c0bf66cb0926934ab39ba5270ffeece7
leowa <leowa@Leow_Laptop1031>
2025-11-18 11:48:31 +0800

    W06E06

--- Exercise2025/src/W06/W06T06.java
+++ Exercise2025/src/W06/W06T06.java
@@ -8,7 +8,6 @@ package W06;
  *
  * @author pensyarah
  */
-import java.util.Random;
 public class W06T06 {
 
     /**
@@ -16,19 +15,8 @@ public class W06T06 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Random rand = new Random();
-        int number = rand.nextInt(256);
-        System.out.print("Binary " + number + ":");
-        int[]binary = new int[8];
-        for(int i =7;i>=0;i--){
-            binary[i]=number%2;
-            number = number/2;
-            
-        }
-        
-        for(int bit:binary){
-            System.out.print(bit);
-        }
-    }
-    
+        String[] str={"Ang","Tan","Fong","Ahmad","Ali"};
+        for (int i=0;i<str.length;i++)
+            System.out.println(str[i]);
+    }   
 }
