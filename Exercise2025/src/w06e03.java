4254df1c19fc6ca3cc1dd658e1809cd4f02c981d
rashaKM2025 <25065905@siswa.um.edu.my>
2025-11-18 11:28:42 +0800

    upd w06e03

---
+++ Exercise2025/src/w06e03.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author acer
+ */
+public class w06e03 {
+    public static void main(String[] args){
+        int num[] = new int[100];
+        num[0] = 12;
+        num[99] = 89;
+        System.out.println(num[0]);
+        System.out.println(num[99]);
+
+    }
+    
+}
