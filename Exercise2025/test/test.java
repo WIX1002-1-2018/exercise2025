cdfcec29c629a01c6d82d01141e28c508dd252ce
星飞 <@Y7000>
2025-11-18 11:26:16 +0800

    test

--- Exercise2025/test/test.java
+++ Exercise2025/test/test.java
@@ -6,20 +6,8 @@
 import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
-	Scanner s=new Scanner(System.in);
-        System.out.print("How many numbers will you enter?: ");
-        int number=s.nextInt();
-        int even=0, odd=0, input;
-        for(int i=1;i<=number;i++){
-            System.out.print("Enter number "+i+": ");
-            input=s.nextInt();
-            if(input%2==0){
-                even++;
-            }else{
-                odd++;
-            }
-        }
-        System.out.println("Total even numbers: "+even);
-        System.out.println("Total odd numbers: "+odd);
+int[] num=new int[100];
+num[0]=12;
+num[99]=89;
     }
 }
