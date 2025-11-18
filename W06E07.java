48fe850d24e36f0a82068b80bf00774c00619494
ANG KAI XIN <23113456@siswa.um.edu.my>
2025-11-18 12:19:29 +0800

    Bug fixes to make the programme actually excutable
    -can print arrays with System.out.println(Arrays.toString(num)); instead of nested loop
    
    -sort array with Arrays.sort(num);

--- W06E07.java
+++ W06E07.java
@@ -1,5 +1,5 @@
 package W06;
-import java.util.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Random;
 
@@ -10,37 +10,45 @@ public class W06E03;
   public static void main(String[] args){
   int size = 20; int j =0;
   int[] num = new int [size];
+  Random random = new Random();
+    
   for ( int i = 0; i < num.length; i++ ) {
-    Random random = new Random();
-    int randomNum = random.nextInt(100);
-    num[j] = randomNum;
-    j + 1;
-    System.out.println("the array is: " + randomNum + "\t"); 
+    num[i] = random.nextInt(100);
   }
-  binarysearch();
+    
+  System.out.println("the array is: "); 
+  System.out.println(Arrays.toString(num)); //practice using this line
+
+  System.out.println("sorted array: "); //practice using this line //critical ogic error: forgot to sort before binary search
+  System.out.println(Arrays.toString(num));
+
+  //call function
+  int result = binarySearch(num);
+  if (result == -1) {System.out.println("Number not found, try another that is in the array");}  
   }
 
   //func to binary search
-  public static int binarysearch() {
+  public static int binarysearch(int[] num) { //goal: to get the position of desired num in the array
     System.out.println("Enter number to search: ");
     Scanner scanner = new scanner(System.in);
     int goal = scanner.nextInt();
 
     int low = 0;
     int high = num.length-1;
-    int middle;
+    //int middle;
 
     while (low <= high) {
-      middle = (low + high)/2;
+      int middle = (low + high)/2;
       if (goal == name[middle]) {
+        System.out.println("Number found at position " + middle);
         return middle; }
       else if (goal < num[middle]) {
         high = middle - 1; }
       else { low = middle + 1; }
     }
-    System.out.println("Found in position " + middle)
+    return -1;
   }
-    
+}    
         
     
     
