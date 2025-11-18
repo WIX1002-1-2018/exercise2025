cfb541056e6bd342e7f7a612f6af2ae110c56441
user <user@LAPTOP-4SOG1HAN>
2025-11-18 16:46:18 +0800

    This is W06E07

---
+++ Exercise2025/src/W06E07/W06E07.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package W06E07;
+import java.util.Random;
+public class W06E07 {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int [] number = new int [20];
+        for (int i=0; i<20; i++){
+            number[i] = r.nextInt(100);
+        }
+        for(int value:number){
+            System.out.print(value+" ");
+        }
+        for (int i=1; i<20; i++){
+            for (int j=0; j<19; j++){
+                if (number[j]>number[j+1]){
+                    int temp = number[j];
+                    number[j] = number[j+1];
+                    number[j+1] = temp;
+                }
+            }
+        }
+
+        System.out.println("\nBubble Sort :");
+        System.out.println("Ascending :");
+        for(int ascending:number){
+            System.out.print(ascending+" ");
+        }
+        System.out.println("\nDescending :");
+        for(int i=19; i>=0;i--){
+            System.out.print(number[i]+" ");
+        }
+    }
+}
