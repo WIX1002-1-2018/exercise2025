bd962b67c177fcceac5fa1ebe6cc1eee39401e8d
rashaKM2025 <25065905@siswa.um.edu.my>
2025-11-18 11:34:13 +0800

    upd w06e04

---
+++ Exercise2025/src/w06e04.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author acer
+ */
+public class w06e04 {
+    public static void main(String[] args){
+        char[] symbol = {'$','%','+','-'};
+        symbol[2] = '*';
+        for (int i = 0; i<symbol.length;i++){
+            System.out.println(symbol[i]);
+        }
+    }
+    
+}
