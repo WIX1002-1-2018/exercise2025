efdbe9a3a07240e2bf4fcb39695734116013c720
丢丢 <@LAPTOP-AVRK3ECR>
2025-11-27 00:37:24 +0800

    try to submit

---
+++ Exercise2025/src/W01/WIX1002.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package W01;
+
+/**
+ *
+ * @author 丢丢
+ */
+public class WIX1002 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
