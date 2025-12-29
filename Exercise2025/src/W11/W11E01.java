ce2b0eabe7a0c1568f90c7e5b05f1c557d7515e9
Badrul Anuar <askbard@gmail.com>
2025-12-29 12:42:58 +0800

    Merge branch 'WIX1002-1-2018:main' into main

---
+++ Exercise2025/src/W11/W11E01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package W11;
+
+/**
+ *
+ * @author Badrul
+ */
+public class W11E01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
